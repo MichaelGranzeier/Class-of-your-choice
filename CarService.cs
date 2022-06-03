be6a170a41c856f5575aa9a2af0ae10911b86639
MichaelGranzeier <granzeierpeter@gmail.com>
2022-06-03 15:14:42 -0400

    finished

---
+++ CarService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_csharp_class_of_your_choice
+{
+    public class CarService
+    {
+        public string Name;
+        public int Year;
+        public int MaintDue;
+        public string ServiceNeeds;
+        public int Miles;
+        public int MaintDate;
+
+        public CarService()
+        {
+            Name = "No Name";
+            Year = 1886;
+            MaintDue = 0;
+            ServiceNeeds = "No service";
+            Miles = 0;
+            MaintDate = 0;
+        }
+
+        public CarService(string name, int year, int maintDue, string serviceNeeds, int miles, int maintDate)
+        {
+            Name = name;
+            Year = year;
+            MaintDue = maintDue;
+            ServiceNeeds = serviceNeeds;
+            Miles = miles;
+            MaintDate = maintDate;
+        }
+
+        public void Drive()
+        {
+            Miles += 10;
+            MaintDue -= 10;
+        }
+        public void Fix()
+        {
+            ServiceNeeds = "No service needed";
+        }
+        public void tick()
+        {
+            MaintDate--;
+            if (MaintDue <= 0)
+            {
+                MaintDue = 1000;
+            }
+            else
+            {
+                MaintDue -= MaintDue / MaintDate;
+                Miles = MaintDue - MaintDate;
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,75 @@
 ﻿// Insert Program Here
+using template_csharp_class_of_your_choice;
+
+Console.WriteLine("Please enter the name of the car.");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter year of the car.");
+int year = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the how many miles until the next maintenance of the car.");
+int maintDue = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the services that are needed.");
+string serviceNeeds = Console.ReadLine();
+Console.WriteLine("Please enter the number of miles on the car.");
+int miles = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the number of day Next Maintenance.");
+int maintDate = Int32.Parse(Console.ReadLine());
+
+CarService car_Service = new CarService(name, year, maintDue, serviceNeeds, miles, maintDate);
+
+bool isRunning = true;
+while (isRunning)
+{
+    Console.Clear();
+    Console.WriteLine("1. Drive \n2. Fix \n3. new day \n4. Display details \n0. Quit");
+    int input;
+    try
+    {
+        input = Int32.Parse(Console.ReadLine());
+    }
+    catch (Exception e)
+    {
+        input = -1;
+    }
+
+
+    switch (input)
+    {
+        case -1:
+            Console.Clear();
+            Console.WriteLine("Please type a numeric value.");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            break;
+        case 0:
+            isRunning = false;
+            break;
+        case 1:
+            car_Service.Drive();
+            Console.Clear();
+            Console.WriteLine(car_Service.MaintDue + " Miles to next maintenance \nThe miles on the car are now " + car_Service.Miles);
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            break;
+        case 2:
+            car_Service.Fix();
+            Console.Clear();
+            Console.WriteLine(car_Service.ServiceNeeds);
+            Console.WriteLine("Press enter to contiue.");
+            Console.ReadLine();
+            break;
+        case 3:
+            car_Service.tick();
+            Console.Clear();
+            Console.WriteLine(car_Service.MaintDue + " Miles and " + car_Service.MaintDate +" days to next maintenance");
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            break;
+        case 4:
+            Console.Clear();
+            Console.WriteLine("Name: " + car_Service.Name + "\nYear: " + car_Service.Year + "\nMiles until the next maintenance of the car:" + car_Service.MaintDue);
+            Console.WriteLine("Maintenance needed: " + car_Service.ServiceNeeds + "\nMiles on the car: " + car_Service.Miles + "\nDays until next maintenance: " + car_Service.MaintDate);
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            break;
+    }
+}
